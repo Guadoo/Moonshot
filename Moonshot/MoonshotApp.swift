87fa4a48b58bda16830bfe1c251faa0389be7663
Liu Jingkai <rock_jk@msn.com>
2021-05-25 17:53:47 +0800

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Guadoo on 2021/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
